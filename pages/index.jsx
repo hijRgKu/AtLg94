9e5fc13b3b5021b54acd21cf9dcac3275ad184b3
hijRgKu <vjaumj@gmail.com>
2022-09-28 10:48:46 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "FNadL6-Uo2nl5Cg5WCVUy12lt6YAYZpY"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "FNadL6-Uo2nl5Cg5WCVUy12lt6YAYZpY"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
